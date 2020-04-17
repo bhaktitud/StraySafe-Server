bbdfb7a46e0ea8ce033c23fd6498de7311d159a0
felixwinangun <felixwinangun@ymail.com>
2020-04-17 11:17:59 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const router = require('./routes');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(router);
+app.use((req, res) => {
+    res.status(404).json({ message: "Not Found!" })
+});
+
+module.exports = app;
