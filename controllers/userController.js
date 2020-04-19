bcf56c9e06edf392c9fdd0fca60d300760b631b8
prmzk <adam.primarizki@gmail.com>
2020-04-19 14:49:10 +0700

    login, register, finduser

---
+++ controllers/userController.js
@@ -0,0 +1,103 @@
+const { User } = require('../models')
+const { verifyPassword } = require('../helpers/bcrypt')
+const { generateToken } = require('../helpers/jwt')
+
+class Controller {
+    static login(req, res, next){
+        const { email, password } = req.body
+        
+        User.findOne({
+            where: {
+                email
+            }
+        })
+        .then(result => {
+            if(!result){
+                throw({
+                    statusCode: 400,
+                    msg: 'Wrong email/password'
+                })
+            }else {
+                if(verifyPassword(password, result.password)){
+                    const token = generateToken(result.id)
+                    res.status(200).json({
+                        token,
+                        first_name: result.first_name,
+                        email: result.email,
+                        img_url: result.img_url,
+                    })
+                }
+                else{
+                    throw ({
+                        statusCode: 400,
+                        msg: 'Wrong email/password'
+                    })
+                }
+            }
+        })
+        .catch(next)
+    }
+
+    static register(req, res, next){
+        const data = {
+            email: req.body.email,
+            password: req.body.password,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            phone_number: req.body.phone_number,
+            bio: req.body.bio,
+            city: req.body.city,
+            img_url: req.body.img_url,
+        }
+
+        User.findOne({
+            where: {
+                email: data.email
+            }
+        })
+        .then(result => {
+            if(result){
+                throw ({
+                    statusCode: 400,
+                    msg: 'Email has already been used'
+                })
+            }
+            else{
+                return User.create(data)
+            }
+        })
+        .then(result => {
+            const token = generateToken(result.id)
+            res.status(200).json({
+                token,
+                first_name: result.first_name,
+                email: result.email,
+                img_url: result.img_url,
+            })
+        })
+        .catch(next)
+        
+    }
+
+    static findUser(req, res, next){
+        const id = req.params.id
+
+        User.findOne({
+            where: {
+                id
+            }
+        })
+        .then(result => {
+            res.status(200).json({
+                first_name: result.first_name,
+                last_name: result.last_name || '',
+                phone_number: result.phone_number,
+                img_url: result.img_url,
+                bio: result.bio || ''
+            })
+        })
+        .catch(next)
+    }
+}
+
+module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const userController = require('../controllers/userController')
 
 router.get('/', function (req, res) {
 	res.send('Welcome StrayCare REST API');
 });
+
+router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.get('/users/:id', userController.findUser)
 
 
 module.exports = router;
