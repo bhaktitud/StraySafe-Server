4c3ed2f8e8bef0474e1da57a931aecfaaa139400
felixwinangun <felixwinangun@ymail.com>
2020-04-20 19:02:52 +0700

    use authorization

--- routes/pet.js
+++ routes/pet.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const Controller = require('../controllers/petController');
 const authentication =  require('../middleware/authentication');
+const authhorization =  require('../middleware/authorization');
 
 router.get('/', Controller.get);
 router.post('/', authentication, Controller.create);
-router.put('/:id', Controller.update);
-router.patch('/:id', Controller.updateRequest);
-router.delete('/:id', Controller.delete);
+router.put('/:id', authentication, authhorization, Controller.update);
+router.patch('/:id', authentication, authhorization, Controller.updateRequest);
+router.delete('/:id', authentication, authhorization, Controller.delete);
 
 module.exports = router;
