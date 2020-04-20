8457c2073f62021c74904f3e0569111dc10d842d
felixwinangun <felixwinangun@ymail.com>
2020-04-21 04:03:25 +0700

    fixing test pet

--- routes/pet.js
+++ routes/pet.js
@@ -3,10 +3,11 @@ const Controller = require('../controllers/petController');
 const authentication =  require('../middleware/authentication');
 const authhorization =  require('../middleware/authorization');
 
-router.get('/', Controller.get);
+router.get('/', authentication, Controller.get);
+router.get('/:id', authentication, Controller.getById);
 router.post('/', authentication, Controller.create);
 router.put('/:id', authentication, authhorization, Controller.update);
-router.patch('/:id', authentication, authhorization, Controller.updateRequest);
+router.patch('/:id', authentication, Controller.updateRequest);
 router.delete('/:id', authentication, authhorization, Controller.delete);
 
 module.exports = router;
